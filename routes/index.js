6eb05bcb5bc04adc88aa50996d3d33a6180ac469
achrams <ramadhanovic@gmail.com>
2020-04-02 20:02:08 +0700

    added meme API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const usersRouter = require('./user')
-
+const memesRouter = require('/meme_api')
 router.get('/', (req, res) => res.send('success connected to server'))
+
 router.use('/users', usersRouter)
+router.use('/meme', memesRouter)
 
 module.exports = router
