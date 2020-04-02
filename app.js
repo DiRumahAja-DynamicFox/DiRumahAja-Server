c3d986c85ad5e42475b43656d5f9eeb62055d0da
robin widjaja <robinsalim@yahoo.com>
2020-04-02 22:33:26 +0700

    resolve conflight package-lock and added cors

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const routes = require('./routes')
+const cors = require('cors')
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors())
 
 app.use('/', routes)
 app.use( (err, req, res, next) => {
