c54b0e603d49680a2354bc030b5c777cb0dea3d4
achrams <ramadhanovic@gmail.com>
2020-04-03 11:48:49 +0700

    fixed all features

--- routes/music_api.js
+++ routes/music_api.js
@@ -4,6 +4,4 @@ const MusicAPIController = require('../controllers/music')
 
 // router.use(authentication)
 router.get('/', MusicAPIController.getMusic)
-router.get('/search', MusicAPIController.getMusicBySearch)
-
 module.exports = router
